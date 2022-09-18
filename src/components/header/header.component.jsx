1b22461dea99fd4d2fafc75cdaa85f99e54634d5
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-18 20:48:30 +0500

    1:00:00

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,13 +11,13 @@ const Header = () => {
                 <h1>Mazan Labeeb</h1>
                 <h5 className="text-light">Full Stack Developer</h5>
                 <CTA />
+                <HeaderSocials />
 
                 <div className="me">
                     <img src={ME} alt="Profile" />
                 </div>
                 
                 <a href="#contact" className="scroll-down">Scroll Dow</a>
-                <HeaderSocials />
             </div>
         </header>
     )
