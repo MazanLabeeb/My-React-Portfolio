155a397b044281ab5cb83348808e257cfad3e070
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-19 02:55:19 +0500

    1:54:43

--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -3,7 +3,20 @@ import "./services.styles.scss";
 const Services = () => {
     return (
         <section id="services">
-            Services
+            <h5>What I Offer</h5>
+            <h2>Services</h2>
+
+            <div className="container services-container">
+                <article className="service">
+                    <div className="service-head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+
+                    <ul className="service-list">
+                        
+                    </ul>
+                </article>
+            </div>
         </section>
     )
 }
