f51d33959e94d1140e552582c758b0b9077acfce
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-20 02:25:22 +0500

    footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -10,18 +10,22 @@ const Footer = () => {
 
             <ul className="permalinks">
                 <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Experience</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Portfolio</a></li>
-                <li><a href="#">Testimonials</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonials">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
 
             <div className="footer-socials">
                 <a href="https://facebook.com/mazanlabeeb"><FaFacebookF /></a>
                 <a href="https://instagram.com/mazanlabeeb"><FiInstagram /></a>
                 <a href="https://twitter.com/mazanlabeeb"><IoLogoTwitter /></a>
+            </div>
+
+            <div className="footer-copyright">
+                <small>&copy; MazanLabeeb.tech All rights reserved.</small>
             </div>
         </footer>
     )
