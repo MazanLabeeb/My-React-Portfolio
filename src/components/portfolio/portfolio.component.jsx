f1f6899389cc63a1a483ae12be6f9bb412056c36
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-21 20:08:59 +0500

    work added

--- src/components/portfolio/portfolio.component.jsx
+++ src/components/portfolio/portfolio.component.jsx
@@ -1,7 +1,7 @@
 import "./portfolio.styles.scss";
-import PORTFOLIO1 from "../../assets/portfolio1.jpg";
-import PORTFOLIO2 from "../../assets/portfolio2.jpg";
-import PORTFOLIO3 from "../../assets/portfolio3.jpg";
+import PORTFOLIO1 from "../../assets/portfolio1.png";
+import PORTFOLIO2 from "../../assets/portfolio2.png";
+import PORTFOLIO3 from "../../assets/portfolio3.png";
 import PORTFOLIO4 from "../../assets/portfolio4.jpg";
 import PORTFOLIO5 from "../../assets/portfolio5.png";
 import PORTFOLIO6 from "../../assets/portfolio6.jpg";
@@ -11,44 +11,23 @@ const data = [
     {
         id: 1,
         image: PORTFOLIO1,
-        title: 'Crypto Currency Dashboard & Financial Visualization',
-        github: "https://www.github.com/mazanlabeeb",
-        demo: "https://mazansblog.herokuapp.com"
+        title: 'Wedding Plan Management System ',
+        github: "https://github.com/MazanLabeeb/WPMS",
+        demo: "https://weddingplanmanagementsystem.herokuapp.com/"
     },
     {
         id: 2,
         image: PORTFOLIO2,
-        title: "Charts templates & infogaphics in Figma",
-        github: "https://www.github.com/mazanlabeeb",
-        demo: "https://mazansblog.herokuapp.com"
+        title: "A basic nodejs based news website (dawn) scraper.",
+        github: "https://github.com/MazanLabeeb/newspk",
+        demo: "https://newspk.herokuapp.com"
     },
     {
         id: 3,
         image: PORTFOLIO3,
-        title: "Figma dashboard UI Kit for data design web apps",
-        github: "https://www.github.com/mazanlabeeb",
-        demo: "https://mazansblog.herokuapp.com"
-    },
-    {
-        id: 4,
-        image: PORTFOLIO4,
-        title: 'Charts templates & infogaphics in Figma',
-        github: "https://www.github.com/mazanlabeeb",
-        demo: "https://mazansblog.herokuapp.com"
-    },
-    {
-        id: 5,
-        image: PORTFOLIO5,
-        title: 'Crypto Currency Dashboard & Financial Visualization',
-        github: "https://www.github.com/mazanlabeeb",
-        demo: "https://mazansblog.herokuapp.com"
-    },
-    {
-        id: 6,
-        image: PORTFOLIO6,
-        title: 'Charts templates & infogaphics in Figma',
-        github: "https://www.github.com/mazanlabeeb",
-        demo: "https://mazansblog.herokuapp.com"
+        title: "Blog made using Expressjs, Mongodb and Bootstrap.",
+        github: "https://github.com/MazanLabeeb/Express-Blog",
+        demo: "https://mazansblog.herokuapp.com/blog?category=20"
     }
 ];
 
@@ -67,7 +46,7 @@ const Portfolio = () => {
                                 <div className="portfolio-item-image">
                                     <img src={image} alt="Portfolio" />
                                 </div>
-                                <h3>{title}</h3>
+                                <h3>{(title)}</h3>
                                 <div className="portfolio-item-cta">
                                     <a href={github} className="btn" target="_blank">Github</a>
                                     <a href={demo} className="btn btn-primary" target="_blank">Live Demo</a>
