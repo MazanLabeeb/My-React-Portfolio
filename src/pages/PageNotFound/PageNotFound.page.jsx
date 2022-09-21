e313fa6874e7cdc575a1c773dbdf9f6854b4d77e
mazanlabeeb <mazanlabeeb95@gmail.com>
2022-09-21 16:42:54 +0500

    404 page added

--- src/pages/PageNotFound/PageNotFound.page.jsx
+++ src/pages/PageNotFound/PageNotFound.page.jsx
@@ -1,11 +1,14 @@
 import "./PageNotFound.styles.scss";
 import {TbFaceIdError} from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
     return (
         <div className="PageNotFound">
-            
             <h1 className="error-heading text-gradient"><TbFaceIdError /> 404</h1>
+            <p>The page you are looking for cannot be found!</p>
+            <br />
+            <Link to={"/"} className="btn btn-primary">Go Home</Link>
         </div>
     );
 }
