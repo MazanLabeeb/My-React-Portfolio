43710dab100aac6fdec1c4692105f877209ca2ad
mazan labeeb <mazanlabeeb95@gmail.com>
2023-01-05 09:38:04 -0800

    Update contact.component.jsx

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
     return (
         <section id="contact">
             <h5>Get In Touch</h5>
-            <h2>Do not Contact Me</h2>
+            <h2>Contact Me</h2>
 
             <div className="container contact-container">
                 <div className="contact-options">
