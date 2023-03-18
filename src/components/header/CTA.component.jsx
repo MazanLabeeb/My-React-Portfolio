f4a29136b9d717ace23640ff7ec68fbb7f20031c
mazanlabeeb <mazanlabeeb95@gmail.com>
2023-03-18 22:55:40 +0500

    emojis added

--- src/components/header/CTA.component.jsx
+++ src/components/header/CTA.component.jsx
@@ -1,9 +1,25 @@
-import CV from "../../assets/cv.pdf";
+// import CV from "../../assets/cv.pdf";
+import { emojisplosion, emojisplosions } from "emojisplosion";
 
 const CTA = () => {
+    const cvHandler = ()=> {
+        // emojisplosion();
+        alert(`Sorry, I'm too lazy to update my CV. So, I removed it.`)
+        var rect = document.getElementById('cv').getBoundingClientRect();
+        var ycoordinate = rect.top;
+        var xcoordinate = rect.left;
+            
+            emojisplosion({
+                position: {
+                    x: xcoordinate + 40,
+                    y: ycoordinate,
+                },
+            });
+    }
     return (
         <div className="cta">
-            <a className="btn" href={CV} download>Download CV</a>
+            <a className="btn" id='cv'  onClick={cvHandler}>Download CV</a>
+            {/* <a className="btn" href={CV} download>Download CV</a> */}
             <a  className="btn btn-primary" href="#contact">Let's Talk</a>
         </div>
     )
