cfc906db2f2db2baea7c9bd124227e9871768a36
Mazan Labeeb <mazanlabeeb95@gmail.com>
2023-05-31 21:40:16 +0500

    Update config.js
    contact removed

--- src/config.js
+++ src/config.js
@@ -10,25 +10,26 @@ export const footerSocialTiles = [
         id: 'link1',
         link: "https://www.upwork.com/freelancers/~01622576d387633e89",
         icon: <SiUpwork />
-    },
-    {
-        id: 'link2',
-        link: "https://www.linkedin.com/in/mazanlabeeb/",
-        icon: <ImLinkedin2 />
-    },
-    {
-        id: 'link3',
-        link: "https://github.com/mazanlabeeb",
-        icon: <FiGithub />
-    },
-    {
-        id: 'link4',
-        link: "https://facebook.com/mazanlabeeb",
-        icon: <FaFacebookF />
-    },
-    {
-        id: 'link5',
-        link: "https://instagram.com/mazanlabeeb",
-        icon: <FiInstagram />
     }
+    // ,
+    // {
+    //     id: 'link2',
+    //     link: "https://www.linkedin.com/in/mazanlabeeb/",
+    //     icon: <ImLinkedin2 />
+    // },
+    // {
+    //     id: 'link3',
+    //     link: "https://github.com/mazanlabeeb",
+    //     icon: <FiGithub />
+    // },
+    // {
+    //     id: 'link4',
+    //     link: "https://facebook.com/mazanlabeeb",
+    //     icon: <FaFacebookF />
+    // },
+    // {
+    //     id: 'link5',
+    //     link: "https://instagram.com/mazanlabeeb",
+    //     icon: <FiInstagram />
+    // }
 ];
