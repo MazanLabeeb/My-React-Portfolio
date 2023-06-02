746deeade3ecc06eeced91ab485be7554057e4eb
Mazan Labeeb <mazanlabeeb95@gmail.com>
2023-06-02 15:16:57 +0500

    Contact Removed

--- src/pages/index/index.page.jsx
+++ src/pages/index/index.page.jsx
@@ -19,7 +19,7 @@ const Index = () => {
             <Services />
             <Portfolio />
             <Testimonials />
-            <Contact />
+           /* <Contact /> */
             <Footer />
         </>
     );
